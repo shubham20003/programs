193977030b2f1478bf3d43d15bd55f1132fa2521
shubham mhase <104731758+shubham20003@users.noreply.github.com>
2024-08-06 19:05:26 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("the first number");
+   scanf("%d",&a);
+   printf("the first number");
+   scanf("%d",&b);
+    int sum = a+b;
+   printf("addition of two numbers %d", sum);
+   
+   }
---
+++ square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    int main()
+    {
+        int side;
+        printf("Enter square side");
+        scanf("%d",&side);
+
+        printf("area is %d",side*side);
+    }
